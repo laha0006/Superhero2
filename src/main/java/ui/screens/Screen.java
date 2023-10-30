3acbff153cc4e890e9879d1fee711e1e1ff0079f
Lars <laha0006@stud.kea.dk>
2023-10-30 22:25:06 +0100

    added controller field and parameter to Base Screen class.

--- src/main/java/ui/screens/Screen.java
+++ src/main/java/ui/screens/Screen.java
@@ -1,10 +1,14 @@
 package ui.screens;
 
+import domain.Controller;
+
 public abstract class Screen {
     protected final String name;
+    protected final Controller controller;
 
-    public Screen(String name) {
+    public Screen(String name,Controller controller) {
         this.name = name;
+        this.controller = controller;
     }
     public abstract boolean show();
 
