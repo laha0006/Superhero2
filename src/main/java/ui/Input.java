e5f6dae5318a20380ce663ffb378cfa0bea17d0c
Lars <laha0006@stud.kea.dk>
2023-10-30 19:40:47 +0100

    added input class from previous version.

---
+++ src/main/java/ui/Input.java
@@ -0,0 +1,72 @@
+package ui;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Input {
+    private final Scanner scanner;
+
+    public Input() {
+        scanner = new Scanner(System.in);
+    }
+
+    public Input(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public String inputString(String prompt) {
+        String output = "";
+        System.out.print(prompt);
+
+        try {
+            //scanner.nextLine();
+            output = scanner.nextLine();
+            return output;
+        } catch (NoSuchElementException e) {
+            return output;
+        }
+    }
+
+    public String inputStringAsInt(String prompt) {
+        String output = "";
+        while(true) {
+            System.out.print(prompt);
+            try {
+                output = scanner.nextLine();
+                int testParse = Integer.parseInt(output);
+                return output;
+            } catch (Exception e) {
+                System.out.println("Must be number!");
+            }
+        }
+    }
+
+    public char inputChar(String prompt) {
+        System.out.print(prompt);
+        while (true) {
+            try {
+                char value = scanner.next().charAt(0);
+                scanner.nextLine();
+                return value;
+            } catch (Exception e) {
+                return '?';
+            }
+        }
+    }
+
+    public int inputInt(String prompt) {
+
+        while (true) {
+            try {
+                System.out.print(prompt);
+                int value = scanner.nextInt();
+                scanner.nextLine();
+                return value;
+            } catch (InputMismatchException e) {
+                System.out.println("Must be number!");
+                scanner.nextLine();
+            }
+        }
+    }
+}
