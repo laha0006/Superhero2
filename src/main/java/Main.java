291a31215c0973a56358b45a1126a028902e6840
Lars <laha0006@stud.kea.dk>
2023-10-30 22:25:38 +0100

    implemented start of application.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
+import domain.Controller;
+import ui.UserInterface;
+
 public class Main {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+        UserInterface ui = new UserInterface(controller);
+        ui.start();
+    }
 }
