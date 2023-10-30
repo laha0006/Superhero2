6b6913448cdec96d8217a9e53a4ac15d3f05399d
Lars <laha0006@stud.kea.dk>
2023-10-30 22:19:05 +0100

    implemented getSupeheroList method. and added final database field.

--- src/main/java/domain/Controller.java
+++ src/main/java/domain/Controller.java
@@ -1,4 +1,14 @@
 package domain;
 
+import java.util.ArrayList;
+
 public class Controller {
+
+    private final Database db;
+    public Controller() {
+        db = new Database();
+    }
+    public ArrayList<Superhero> getSuperheroList() {
+        return db.getSuperheroList();
+    }
 }
