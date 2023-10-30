6f79cd9946d9ef7c8ca54e2bd2143c9b88b95168
Lars <laha0006@stud.kea.dk>
2023-10-30 21:44:27 +0100

    implemented MainMenu table screen.

--- src/main/java/ui/screens/MainMenu.java
+++ src/main/java/ui/screens/MainMenu.java
@@ -1,11 +1,43 @@
 package ui.screens;
 
+import ui.Input;
+import ui.table.Row;
+import ui.table.Table;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainMenu extends Screen {
+    private final ArrayList<Screen> screens;
     public MainMenu(String name) {
         super(name);
+        screens = new ArrayList<>();
     }
+
+    public MainMenu(String name,ArrayList screens) {
+        super(name);
+        this.screens = screens;
+    }
+
+    public void addScreen(Screen screen) {
+        screens.add(screen);
+    }
+
     @Override
     public boolean show() {
-        return false;
+         Table table = new Table(name, new ArrayList<>(List.of("#", "Option")),true);
+            int count = 1;
+            for (Screen screen : screens) {
+                Row row = new Row();
+                //System.out.println(count + ". " + screen.getName());
+                row.addCell(""+count).addCell(screen.getName());
+                table.addRow(row);
+                count++;
+            }
+            table.addRow(new Row().addCell(""+0).addCell("To go BACK or EXIT"));
+            System.out.println(table.getTableString());
+            int choice = Input.inputInt(": ");
+            if (choice == 0) return false;
+            screens.get(choice - 1).show();
     }
 }
