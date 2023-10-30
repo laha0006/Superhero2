025ff3e4df790b940f73606ee78f6ca7685a0196
Lars <laha0006@stud.kea.dk>
2023-10-30 22:19:23 +0100

    added showDatabase screen.

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,15 +1,20 @@
 package ui;
 
+import domain.Controller;
 import ui.screens.MainMenu;
 import ui.screens.Screen;
+import ui.screens.ShowDatabase;
 
 public class UserInterface {
 
-    public UserInterface() {
-
+    private final Controller controller;
+    public UserInterface(Controller controller) {
+        this.controller = controller;
     }
     public void start() {
-        Screen mainMenu = new MainMenu("Superhero v2");
+        MainMenu mainMenu = new MainMenu("Superhero v2");
+        ShowDatabase showDatabase = new ShowDatabase("Show Database",controller);
+        mainMenu.addScreen(showDatabase);
         boolean run = true;
         while(run) {
             run = mainMenu.show();
