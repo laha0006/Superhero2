b4b9ea07730631218ceb66b4e2254ac073c5614d
Lars <laha0006@stud.kea.dk>
2023-10-30 19:40:59 +0100

    added MainMenu class.

---
+++ src/main/java/ui/screens/MainMenu.java
@@ -0,0 +1,11 @@
+package ui.screens;
+
+public class MainMenu extends Screen {
+    public MainMenu(String name) {
+        super(name);
+    }
+    @Override
+    public boolean show() {
+        return false;
+    }
+}
