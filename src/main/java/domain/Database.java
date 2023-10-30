b823e4c3292008104fa67974d8b64e2fe15897b5
Lars <laha0006@stud.kea.dk>
2023-10-30 15:14:01 +0100

    Added Database and Superhero Class from previous version

---
+++ src/main/java/domain/Database.java
@@ -0,0 +1,55 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class Database {
+    private final ArrayList<Superhero> superheroList;
+
+    public Database() {
+        superheroList = new ArrayList<>();
+        superheroList.add(new Superhero("Superman", "Clark Kent", false, "Super.", 1950, 999));
+        superheroList.add(new Superhero("Batman", "Bruce Wayne", true, "Bat", 1940, 100));
+        superheroList.add(new Superhero("Arrow", "Joe Spencer", true, "Arrow guy", 1950, 100));
+        superheroList.add(new Superhero("Spiderman", "Peter Parker", true, "Spider allergy", 1930, 88));
+        superheroList.add(new Superhero("Fat Fury", "Herbie Popnecker", true, "Fat fury, what more to say", 1958, 200));
+    }
+
+    public void addSuperhero(Superhero superhero) {
+        superheroList.add(superhero);
+    }
+
+    public Superhero search(String superheroName) {
+        for (Superhero superhero : superheroList) {
+            if (superhero.getName().toLowerCase().contains(superheroName.toLowerCase())) {
+                return superhero;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Superhero> searchMany(String superheroName) {
+        ArrayList<Superhero> superheroes = new ArrayList<>();
+        for (Superhero superhero : superheroList) {
+            if (superhero.getName().toLowerCase().contains(superheroName.toLowerCase())) {
+                superheroes.add(superhero);
+            }
+        }
+        return superheroes;
+    }
+
+    public ArrayList<Superhero> getSuperheroList() {
+        return superheroList;
+    }
+
+    @Override
+    public String toString() {
+        return "superhero.Database{" +
+                "superheroList=" + superheroList +
+                '}';
+    }
+
+    public void delete(int index) {
+        superheroList.remove(index);
+    }
+
+}
