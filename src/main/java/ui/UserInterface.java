743ac446748dbea073ad1af4ff7e73636041dba4
Lars <laha0006@stud.kea.dk>
2023-10-30 21:44:00 +0100

    implemented the basic application loop

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -1,4 +1,18 @@
 package ui;
 
+import ui.screens.MainMenu;
+import ui.screens.Screen;
+
 public class UserInterface {
+
+    public UserInterface() {
+
+    }
+    public void start() {
+        Screen mainMenu = new MainMenu("Superhero v2");
+        boolean run = true;
+        while(run) {
+            run = mainMenu.show();
+        }
+    }
 }
