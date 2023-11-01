8e5075e6889521a48e01fd21a2e4e5cbfa3d27f6
Lars <laha0006@stud.kea.dk>
2023-11-01 05:53:21 +0100

    main updated due to testing.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
+import data.Database;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
 import domain.Controller;
+import domain.Superhero;
 import ui.UserInterface;
 
 public class Main {
@@ -6,5 +16,12 @@ public class Main {
         Controller controller = new Controller();
         UserInterface ui = new UserInterface(controller);
         ui.start();
+//        ArrayList<Superhero> superheroes = controller.getSuperheroList();
+//        Database db = new Database();
+//        db.writeToCsv(superheroes);
+
+
+
+
     }
 }
