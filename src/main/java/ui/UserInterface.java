615871cbe5d4f0a028866de4bb545b9dbfb15d80
Lars <laha0006@stud.kea.dk>
2023-11-06 14:47:14 +0100

    updated userinterface with ShowSortedDatabase and SearchDatabase Screens

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -14,10 +14,14 @@ public class UserInterface {
         Screen showDatabase = new ShowDatabase("Show Database",controller);
         Screen addSuphero = new AddSuperhero("Add Supehero",controller);
         Screen editSuphero = new EditSuperhero("Edit Supehero",controller);
+        Screen searchDatabase = new SearchDatabase("Search Superhero", controller);
+        Screen showSortedDatabase = new ShowSortedDatabase("Show Sorted Database",controller);
         Screen deleteSuperhero = new DeleteSuperhero("Delete Superhero", controller);
         mainMenu.addScreen(showDatabase);
+        mainMenu.addScreen(showSortedDatabase);
         mainMenu.addScreen(addSuphero);
         mainMenu.addScreen(editSuphero);
+        mainMenu.addScreen(searchDatabase);
         mainMenu.addScreen(deleteSuperhero);
 
         boolean run = true;
