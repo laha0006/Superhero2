972e940f2dc3ddb61a3c6106c6ae8bbd962a54ed
Lars <laha0006@stud.kea.dk>
2023-11-06 15:17:48 +0100

    Implemented comparator.

--- src/main/java/domain/comparators/SuperheroYearCreatedComparator.java
+++ src/main/java/domain/comparators/SuperheroYearCreatedComparator.java
@@ -1,4 +1,12 @@
 package domain.comparators;
 
-public class SuperheroYearCreatedComparator {
+import domain.Superhero;
+
+import java.util.Comparator;
+
+public class SuperheroYearCreatedComparator implements Comparator<Superhero> {
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return Integer.compare(o1.getCreationYear(), o2.getCreationYear());
+    }
 }
