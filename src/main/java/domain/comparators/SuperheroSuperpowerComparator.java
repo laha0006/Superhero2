33dc8d304c0428e4f6046bdb273e98cbb13f9aad
Lars <laha0006@stud.kea.dk>
2023-11-06 15:17:40 +0100

    Implemented comparator.

--- src/main/java/domain/comparators/SuperheroSuperpowerComparator.java
+++ src/main/java/domain/comparators/SuperheroSuperpowerComparator.java
@@ -1,4 +1,12 @@
 package domain.comparators;
 
-public class SuperheroSuperpowerComparator {
+import domain.Superhero;
+
+import java.util.Comparator;
+
+public class SuperheroSuperpowerComparator implements Comparator<Superhero> {
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return o2.getSuperPower().compareTo(o2.getSuperPower());
+    }
 }
