dacd2703d343fc70bbe42a90be67fbcfab9ac8db
Lars <laha0006@stud.kea.dk>
2023-11-06 15:16:59 +0100

    Implemented comparator.

--- src/main/java/domain/comparators/SuperheroIsHumanComparator.java
+++ src/main/java/domain/comparators/SuperheroIsHumanComparator.java
@@ -1,4 +1,12 @@
 package domain.comparators;
 
-public class SuperheroIsHumanComparator {
+import domain.Superhero;
+
+import java.util.Comparator;
+
+public class SuperheroIsHumanComparator implements Comparator<Superhero> {
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return Boolean.compare(o1.isHuman(),o2.isHuman())
+    }
 }
