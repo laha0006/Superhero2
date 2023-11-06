6e49d81350f4e1100ef311c95eaaefe2f516c927
Lars <laha0006@stud.kea.dk>
2023-11-06 15:17:06 +0100

    Implemented comparator.

--- src/main/java/domain/comparators/SuperheroNameComparator.java
+++ src/main/java/domain/comparators/SuperheroNameComparator.java
@@ -7,6 +7,6 @@ import java.util.Comparator;
 public class SuperheroNameComparator implements Comparator<Superhero> {
     @Override
     public int compare(Superhero o1, Superhero o2) {
-        return 0;
+        return o1.getName().compareTo(o2.getName());
     }
 }
