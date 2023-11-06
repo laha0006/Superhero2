45f7f975f00702cd38763b0b7ded3944b80f7ce6
Lars <laha0006@stud.kea.dk>
2023-11-06 15:03:10 +0100

    addded comparator.

---
+++ src/main/java/domain/comparators/SuperheroNameComparator.java
@@ -0,0 +1,12 @@
+package domain.comparators;
+
+import domain.Superhero;
+
+import java.util.Comparator;
+
+public class SuperheroNameComparator implements Comparator<Superhero> {
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return 0;
+    }
+}
