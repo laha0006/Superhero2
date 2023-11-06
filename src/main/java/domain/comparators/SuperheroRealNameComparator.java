3d0da04a700f958e94d2e3f6dd6d8bf00b56873f
Lars <laha0006@stud.kea.dk>
2023-11-06 15:17:11 +0100

    Implemented comparator.

--- src/main/java/domain/comparators/SuperheroRealNameComparator.java
+++ src/main/java/domain/comparators/SuperheroRealNameComparator.java
@@ -1,4 +1,12 @@
 package domain.comparators;
 
-public class SuperheroRealNameComparator {
+import domain.Superhero;
+
+import java.util.Comparator;
+
+public class SuperheroRealNameComparator implements Comparator<Superhero> {
+    @Override
+    public int compare(Superhero o1, Superhero o2) {
+        return o1.getRealName().compareTo(o2.getRealName());
+    }
 }
