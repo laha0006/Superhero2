2a5549c0fa01131cda6c6d958eb634f780ad78d7
Lars <laha0006@stud.kea.dk>
2023-11-07 15:03:31 +0100

    implemented overloaded show method for sorting purposes

--- src/main/java/ui/screens/ShowDatabase.java
+++ src/main/java/ui/screens/ShowDatabase.java
@@ -15,14 +15,32 @@ public class ShowDatabase extends Screen {
         super(name,controller);
 
     }
-    public void showDatabase() throws Exception {
+    public void showDatabase() {
         ArrayList<Superhero> superheroes = controller.getSuperheroList();
         Table table = new Table("Superheros",
                 new ArrayList<>(List.of("Superhero Name","Real Name",
                         "Human?","Superpower","Strength","Year Created")),true);
         for (Superhero hero : superheroes) {
             table.addRow(new Row().addCell(hero.getName())
-                    .addCell(hero.getRealName(), Color.ORANGE)
+                    .addCell(hero.getRealName())
+                    .addCell(hero.isHuman())
+                    .addCell(hero.getSuperPower())
+                    .addCell(hero.getStrength())
+                    .addCell(hero.getCreationYear()));
+        }
+        System.out.println(table.getTableString());
+    }
+
+
+    public void showDatabase(int primary,int secondary, int order1, int order2) {
+        ArrayList<Superhero> superheroes = controller.getSuperheroList();
+        ArrayList<String> columns = new ArrayList<>(List.of("Superhero Name","Real Name",
+                        "Human?","Superpower","Strength","Year Created"));
+        Table table = new Table("Superheros",columns ,true);
+        table.setSortHighlight(primary,secondary,order1,order2);
+        for (Superhero hero : superheroes) {
+            table.addRow(new Row().addCell(hero.getName())
+                    .addCell(hero.getRealName())
                     .addCell(hero.isHuman())
                     .addCell(hero.getSuperPower())
                     .addCell(hero.getStrength())
@@ -35,6 +53,16 @@ public class ShowDatabase extends Screen {
     public boolean show() {
         try {
             showDatabase();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        Input.back();
+        return false;
+    }
+
+    public boolean show(int primary, int secondary,int order1, int order2) {
+        try {
+            showDatabase(primary,secondary,order1,order2);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
